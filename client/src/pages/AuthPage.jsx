3b87a94335ea97ce8e6b38717e68178acb8e5d50
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-03 10:06:09 -0400

    created AuthPage component for handling user authentication. dynamically rendered the form fields and corresponding titles based on the authValue stored in the localStorage to know whether the user is on the login or signup page. set up formInputs state for managing email, password and confirmPassword. set up handleInputChange function to update the form input states whenever the user types in the form fields. once form is submitted, sends a POST request to the server with the form data based on the authValue login or signup

---
+++ client/src/pages/AuthPage.jsx
@@ -0,0 +1,102 @@
+import { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import axios from 'axios';
+
+export default function AuthPage(props) {
+  const authValue = localStorage.getItem('authValue');
+  const { storeAuthValue } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [formInputs, setFormInputs] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+
+    setFormInputs(prevState => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+
+  async function submitHandler(e) {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(`/api/auth/${authValue}`, {
+        email: formInputs.email,
+        password: formInputs.password,
+      });
+      console.log(res);
+
+      if (authValue === 'login' && res.status === 200) {
+        navigate('/dashboard');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <section>
+      <div>
+        <h2>{authValue === 'login' ? 'Login' : 'Create an Account'}</h2>
+
+        <form onSubmit={submitHandler}>
+          <label>
+            Email Address
+            <input
+              type="email"
+              name="email"
+              placeholder="name@email.com"
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              name="password"
+              placeholder="••••••••"
+              onChange={handleInputChange}
+            />
+          </label>
+          {authValue === 'signup' && (
+            <label>
+              Confirm Password
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="••••••••"
+                onChange={handleInputChange}
+              />
+            </label>
+          )}
+          <button type="submit">
+            {authValue === 'login' ? 'Login' : 'Signup'}
+          </button>
+        </form>
+
+        <p>
+          {authValue === 'signup'
+            ? 'Already have an account? '
+            : "Don't have an account? "}
+          <Link
+            to={authValue === 'signup' ? '/login' : '/signup'}
+            onClick={() =>
+              storeAuthValue(authValue === 'signup' ? 'login' : 'signup')
+            }
+          >
+            {authValue === 'signup' ? 'Login' : 'Signup'} Now
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+}
