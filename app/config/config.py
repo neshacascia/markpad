4701bf76c4ca5120b4777aeec0a283e357a30167
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-03 15:52:00 -0400

    updated configurations to use cookies for storing JWTs and ensured they are set correctly for cross-site requests

--- app/config/config.py
+++ app/config/config.py
@@ -9,3 +9,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'anothersecret')
+    JWT_TOKEN_LOCATION = ['cookies']
+    JWT_COOKIE_CSRF_PROTECT = True  
+    JWT_COOKIE_SAMESITE = 'None'     
+    JWT_COOKIE_SECURE = True   
