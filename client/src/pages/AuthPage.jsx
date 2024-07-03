b33a775784f2928a462c9bad69ba34eddb6c4d41
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-03 10:11:12 -0400

    after POST request, check if the status code is 201 or 200 to redirect users to /dashboard route

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -35,7 +35,7 @@ export default function AuthPage(props) {
       });
       console.log(res);
 
-      if (authValue === 'login' && res.status === 200) {
+      if (res.status === 200 || res.status === 201) {
         navigate('/dashboard');
       }
     } catch (err) {
