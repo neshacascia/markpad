0a38985bc5230ca9800927d9401875698ef7c6bf
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-03 22:39:29 -0400

    refactored

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -3,6 +3,15 @@ from ..controllers import auth_controller
 
 auth_bp = Blueprint('auth_bp', __name__)
 
-auth_bp.route('/api/auth/signup', methods=['POST'])(auth_controller.post_signup)
-auth_bp.route('/api/auth/login', methods=['POST'])(auth_controller.post_login)
-auth_bp.route('/api/auth/logout', methods=['GET'])(auth_controller.logout)
+@auth_bp.route('/api/auth/signup', methods=['POST'])
+def post_signup():
+    return auth_controller.post_signup
+
+@auth_bp.route('/api/auth/login', methods=['POST'])
+def post_login():
+    return auth_controller.post_login
+
+@auth_bp.route('/api/auth/logout', methods=['GET'])
+def logout():
+    return auth_controller.logout
+
