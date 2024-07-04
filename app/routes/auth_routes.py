707d78dae8b22bba903e0f99aec88f64f3b8e1c3
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-03 22:45:03 -0400

    fixed error

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -5,13 +5,13 @@ auth_bp = Blueprint('auth_bp', __name__)
 
 @auth_bp.route('/api/auth/signup', methods=['POST'])
 def post_signup():
-    return auth_controller.post_signup
+    return auth_controller.post_signup()
 
 @auth_bp.route('/api/auth/login', methods=['POST'])
 def post_login():
-    return auth_controller.post_login
+    return auth_controller.post_login()
 
 @auth_bp.route('/api/auth/logout', methods=['GET'])
 def logout():
-    return auth_controller.logout
+    return auth_controller.logout()
 
