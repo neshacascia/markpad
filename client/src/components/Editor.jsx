dd0e1ebb68839d8f672708e2743f0c85c601c1ff
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-10 10:09:15 -0400

    updated to handleInputChange function which uses the name to update the document object with the user input. added rows and cols attributes to the textarea input field

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,11 +1,22 @@
-export default function Editor({ markdown, setMarkdown }) {
-  function handleEditorChange(e) {
-    setMarkdown(e.target.value);
+export default function Editor({ document, setDocument }) {
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+
+    setDocument(prevDocument => ({
+      ...prevDocument,
+      [name]: value,
+    }));
   }
 
   return (
     <section>
-      <textarea value={markdown} onChange={handleEditorChange} />
+      <textarea
+        name="content"
+        value={document.content}
+        onChange={handleInputChange}
+        rows={10}
+        cols={50}
+      />
     </section>
   );
 }
