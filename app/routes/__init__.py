ba14d012c9212b2166005518696695146ba4b0a7
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-10 10:18:50 -0400

    registered document_bp as blueprint route and created route to /api/document/saveDocument which accepts a POST request and runs the save_document controller function

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from app.routes.user_routes import user_bp
 from app.routes.auth_routes import auth_bp
+from app.routes.document_routes import document_bp
 
 
 def register_routes(app):
     app.register_blueprint(user_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(document_bp)
