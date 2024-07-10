a8b80c5ba57527b013d9919b193fe4c81347c9a6
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-10 10:26:32 -0400

    wrapped context providers around app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthContextProvider } from './context/AuthContext.jsx';
+import { UIContextProvider } from './context/UIContext.jsx';
+import { DocumentContextProvider } from './context/DocumentContext.jsx';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <UIContextProvider>
+      <DocumentContextProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </DocumentContextProvider>
+    </UIContextProvider>
   </AuthContextProvider>
 );
