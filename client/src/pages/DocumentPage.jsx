ebf6019f1b532d6936c5a9680988ea607fbb7413
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-10 10:26:13 -0400

    render Sidebar component in DocumentPage when displaySidebar is true

--- client/src/pages/DocumentPage.jsx
+++ client/src/pages/DocumentPage.jsx
@@ -3,14 +3,17 @@ import { Navigate } from 'react-router-dom';
 import { getCookie } from '../utils/cookies';
 import { AuthContext } from '../context/AuthContext';
 import { DocumentContext } from '../context/DocumentContext';
+import { UIContext } from '../context/UIContext';
 import Editor from '@components/Editor';
 import Preview from '@components/Preview';
+import Sidebar from '../components/Sidebar';
 
 import { defaultMarkdown } from '../defaultMarkdown';
 
 export default function DocumentPage() {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
   const { document, setDocument } = useContext(DocumentContext);
+  const { displaySidebar } = useContext(UIContext);
 
   useEffect(() => {
     if (getCookie('csrf_access_token')) {
@@ -27,6 +30,7 @@ export default function DocumentPage() {
           <Editor document={document} setDocument={setDocument} />{' '}
           <span>Preview</span>
           <Preview markdown={document.content} />
+          {displaySidebar && <Sidebar />}
         </section>
       ) : (
         <Navigate to="/" />
