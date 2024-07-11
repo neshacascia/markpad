dae784db066275840fa39811998e1c7d94187786
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-11 08:56:58 -0400

    created Document component which uses an useEffect to make parallel request to fetch document data and all documents if an id is present in the URL. it updates the corresponding states with the data and is passed down as props to Editor and Preview components. defined the loadingData state to manage loading status, initially set to true. after request is completed, set back to false. used state to conditionally render the loading message or the section containing the Editor and Preview components

---
+++ client/src/components/Document.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { DocumentContext } from '../context/DocumentContext';
+import { AuthContext } from '../context/AuthContext';
+import { UIContext } from '../context/UIContext';
+import Editor from './Editor';
+import Preview from './Preview';
+import Sidebar from './Sidebar';
+import { getCookie } from '../utils/cookies';
+import axios from 'axios';
+
+export default function Document() {
+  const { id } = useParams();
+  const { document, setDocument, setAllDocuments } =
+    useContext(DocumentContext);
+  const { setIsLoggedIn } = useContext(AuthContext);
+  const { displaySidebar } = useContext(UIContext);
+  const [loadingData, setLoadingData] = useState(true);
+
+  useEffect(() => {
+    if (getCookie('csrf_access_token')) {
+      setIsLoggedIn(true);
+    }
+
+    if (id) {
+      async function fetchData() {
+        try {
+          const [documentRes, allDocumentsRes] = await Promise.all([
+            axios.get(`/api/document/${id}`, {
+              withCredentials: true,
+              headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': getCookie('csrf_access_token'),
+              },
+            }),
+            axios.get('/api/document/getAllDocuments', {
+              withCredentials: true,
+              headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': getCookie('csrf_access_token'),
+              },
+            }),
+          ]);
+
+          const { document } = documentRes.data;
+          const { documents } = allDocumentsRes.data;
+
+          setDocument(document);
+          setAllDocuments(documents);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setLoadingData(false);
+        }
+      }
+      fetchData();
+    }
+  }, [id]);
+
+  return (
+    <main>
+      {loadingData ? (
+        <p>Loading...</p>
+      ) : (
+        <section>
+          <span>Markdown</span>
+          <Editor document={document} setDocument={setDocument} />{' '}
+          <span>Preview</span>
+          <Preview markdown={document.content} />
+        </section>
+      )}
+      {displaySidebar && <Sidebar />}
+    </main>
+  );
+}
