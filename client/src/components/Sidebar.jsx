92893541b937afbe7b9f2e8caed165af0d4e4fed
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-11 08:46:00 -0400

    created const documentList which generates each document to a Link from allDocuments state array. upon clicking the link, it sets document to the current document and closes the sidebar. also rendered the document list inside the Sidebar component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import { useContext } from 'react';
 import { DocumentContext } from '../context/DocumentContext';
 import { UIContext } from '../context/UIContext';
+import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
-  const { setDocument } = useContext(DocumentContext);
+  const { setDocument, allDocuments } = useContext(DocumentContext);
   const { setDisplaySidebar } = useContext(UIContext);
 
   function createNewDocument() {
@@ -11,11 +12,25 @@ export default function Sidebar() {
     setDisplaySidebar(false);
   }
 
+  const documentList = allDocuments.map(document => (
+    <Link
+      to={`/document/${document.id}`}
+      key={document.id}
+      onClick={() => {
+        setDocument(document);
+        setDisplaySidebar(false);
+      }}
+    >
+      {document.name}
+    </Link>
+  ));
+
   return (
     <div>
       <span>Markpad</span>
       <div>
         <span>My Documents</span>
+        {documentList}
         <button onClick={createNewDocument}>New Document</button>
       </div>
     </div>
