5a9f2480692a586c740e1af36d4f1b77dfe72820
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-12 12:25:44 -0400

    created handleDeleteDocument function which makes DELETE request to /document/deleteDocument with the current document's id as the query parameter

--- client/src/components/document/Delete.jsx
+++ client/src/components/document/Delete.jsx
@@ -1,9 +1,30 @@
 import { useContext } from 'react';
 import { DocumentContext } from '../../context/DocumentContext';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { getCookie } from '../../utils/cookies';
 
 export default function Delete() {
   const { document } = useContext(DocumentContext);
+  const navigate = useNavigate();
 
+  async function handleDeleteDocument() {
+    try {
+      const res = await axios.delete(
+        `/api/document/deleteDocument/${document.id}`,
+        {
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': getCookie('csrf_access_token'),
+          },
+        }
+      );
+      console.log(res);
+    } catch (err) {
+      console.error(err);
+    }
+  }
   return (
     <div>
       <h2>Delete this document?</h2>
@@ -11,7 +32,7 @@ export default function Delete() {
         Are you sure you want to delete the {`'${document.name}'`} document and
         its contents? This action cannot be reversed.
       </p>
-      <button>Confirm & Delete</button>
+      <button onClick={handleDeleteDocument}>Confirm & Delete</button>
     </div>
   );
 }
