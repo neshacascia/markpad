f0f34392bf183e5e1dbc89290838145364f6e0ac
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-12 10:47:23 -0400

    created new modal state and implemented two functions: openModal which will update the modal state with the modalKey passed in and closeModal which will set the modal to null

--- client/src/context/UIContext.jsx
+++ client/src/context/UIContext.jsx
@@ -4,9 +4,27 @@ const UIContext = createContext();
 
 function UIContextProvider(props) {
   const [displaySidebar, setDisplaySidebar] = useState(false);
+  const [modal, setModal] = useState(null);
+
+  function openModal(modalKey) {
+    setModal(modalKey);
+  }
+
+  function closeModal() {
+    setModal(null);
+  }
 
   return (
-    <UIContext.Provider value={{ displaySidebar, setDisplaySidebar }}>
+    <UIContext.Provider
+      value={{
+        displaySidebar,
+        setDisplaySidebar,
+        modal,
+        setModal,
+        openModal,
+        closeModal,
+      }}
+    >
       {props.children}
     </UIContext.Provider>
   );
