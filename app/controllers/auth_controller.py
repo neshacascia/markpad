124aa383e931c9f8c349ecb06c2be2d693115cc6
Nesha Mervin <nesha.mervin@gmail.com>
2024-07-31 11:40:13 -0400

    updated post_login controller to combine demo user and regular user check into a single conditional statement, to avoid repeating the process of creating access token and setting cookies. enables automatic login for demo users

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -2,6 +2,7 @@ from flask import request, jsonify, make_response
 from flask_jwt_extended import create_access_token, set_access_cookies, unset_jwt_cookies
 from ..models.user import User
 from ..extensions import db
+import os
 
 def post_signup():
     data = request.get_json()
@@ -24,12 +25,14 @@ def post_signup():
 def post_login():
     data = request.get_json()
     user = User.query.filter_by(email=data['email']).first()
-    if user and user.check_password(data['password']):
+
+    if data['email'] == os.getenv('DEMO_USER_EMAIL') or (user and user.check_password(data['password'])):
         access_token = create_access_token(identity=user.id)
         response = make_response(jsonify({'msg': 'Success! You are logged in.'}), 200)
         set_access_cookies(response, access_token)
 
-        return response
+        return response 
+
     return jsonify({'msg': 'Invalid email or password. Please try again.'}), 401
 
 def logout():
